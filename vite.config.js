52ce7d853cd8ebcc4659962b6cf114b9ea2e9619
Biki Kalita <86558912+Biki-dev@users.noreply.github.com>
2025-03-16 07:37:12 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Assamese-Heritage-and-Handloom-E-Commerce-1/', // Replace with your repo name
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-  }
-})
+  },
+});
